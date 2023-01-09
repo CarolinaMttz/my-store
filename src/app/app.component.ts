c4a2816ed05ce153576b4c8f36109082d5496950
Dulce Carolina Martínez Carrera <dulce.martinez@ulsaoaxaca.edu.mx>
2023-01-09 13:27:33 -0600

    Clase Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,13 @@ export class AppComponent {
     age:    18,
     avatar: 'https://www.lavanguardia.com/files/og_thumbnail/uploads/2017/12/08/5fa3d8e24f841.jpeg'
   }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge(){
+    this.person.age += 1;
+  }
+
 }
